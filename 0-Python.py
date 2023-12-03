afe14004ce4227f473b1cf85c325d878b45afb11
shubhpatel2802 <shubhpatel2802@gmail.com>
2023-12-03 18:58:09 +0530

    0-Python.py"

---
+++ 0-Python.py
@@ -0,0 +1,147 @@
+'''
+1] What is Python?
+->Python is a high-level, general-purpose programming language that emphasizes readability and simplicity. 
+It was created by Guido van Rossum and first released in 1991. Python is designed to be easy to understand 
+and write, with a clean and straightforward syntax that allows developers to express concepts in fewer lines 
+of code than languages like C++ or Java.
+
+2] Python Features.
+-> Python is a versatile and powerful programming language with a variety of features that contribute to its popularity. 
+Here are some key features of Python:
+
+1. **Easy to Learn and Read:**
+   - Python's syntax is designed to be simple and readable, making it easy for beginners to learn and understand.
+
+2. **Expressive Language:**
+   - Python allows developers to express ideas in fewer lines of code compared to other languages, leading to increased productivity.
+
+3. **Interpreted and Interactive:**
+   - Python is an interpreted language, meaning that code is executed line by line, making development and debugging easier. 
+   It also supports an interactive mode where code can be executed and tested interactively.
+
+4. **High-Level Language:**
+   - Python is a high-level language, abstracting many low-level details and providing a more straightforward and concise 
+   programming experience.
+
+5. **Dynamically Typed:**
+   - Python is dynamically typed, which means that variable types are determined at runtime, allowing for more flexibility 
+   and ease of development.
+
+6. **Large Standard Library:**
+   - Python comes with a comprehensive standard library that includes modules and packages for various tasks, such as file I/O, 
+   networking, regular expressions, and more. This reduces the need for developers to write code from scratch for common 
+   functionalities.
+
+7. **Community and Ecosystem:**
+   - Python has a large and active community of developers who contribute to the language's growth. The Python Package Index (PyPI)
+   hosts a vast collection of third-party libraries and frameworks that extend Python's capabilities.
+
+8. **Cross-Platform:**
+   - Python is compatible with various operating systems, including Windows, macOS, and Linux, allowing developers to write code 
+   that can run on different platforms without modification.
+
+9. **Object-Oriented:**
+   - Python supports object-oriented programming (OOP) principles, including encapsulation, inheritance, and polymorphism, 
+   making it suitable for building modular and reusable code.
+
+10. **Extensibility:**
+    - Python can be easily extended by integrating code written in languages like C or C++, allowing for performance-critical 
+    tasks to be implemented in these languages.
+
+11. **Versatility in Application:**
+    - Python is used in a wide range of applications, including web development, data science, machine learning, artificial 
+    intelligence, automation, scientific computing, and more.
+
+12. **Support for Integration:**
+    - Python can be easily integrated with other languages and technologies, making it a popular choice for building systems 
+    that involve multiple technologies.
+
+3] Python Operator.
+-> In Python, operators are symbols or special keywords used to perform operations on variables and values. Python supports a wide range of operators, and they can be categorized into various types, such as arithmetic operators, comparison operators, logical operators, assignment operators, and more. Here's an overview of some common types of operators in Python:
+
+1. **Arithmetic Operators:**
+   - `+` (Addition)
+   - `-` (Subtraction)
+   - `*` (Multiplication)
+   - `/` (Division)
+   - `%` (Modulus)
+   - `**` (Exponentiation)
+   - `//` (Floor Division)
+
+   Ex:-
+   a = 10
+   b = 3
+   print(a + b)  # Output: 13
+   print(a - b)  # Output: 7
+   print(a * b)  # Output: 30
+   print(a / b)  # Output: 3.333...
+   print(a % b)  # Output: 1
+   print(a ** b) # Output: 1000
+   print(a // b) # Output: 3
+   
+
+2. **Comparison Operators:**
+   - `==` (Equal to)
+   - `!=` (Not equal to)
+   - `<` (Less than)
+   - `>` (Greater than)
+   - `<=` (Less than or equal to)
+   - `>=` (Greater than or equal to)
+
+   Ex:-
+   x = 5
+   y = 10
+   print(x == y)  # Output: False
+   print(x != y)  # Output: True
+   print(x < y)   # Output: True
+   print(x > y)   # Output: False
+   print(x <= y)  # Output: True
+   print(x >= y)  # Output: False
+   
+
+3. **Logical Operators:**
+   - `and` (Logical AND)
+   - `or` (Logical OR)
+   - `not` (Logical NOT)
+
+   Ex:-
+   p = True
+   q = False
+   print(p and q)  # Output: False
+   print(p or q)   # Output: True
+   print(not p)     # Output: False
+   
+
+4. **Assignment Operators:**
+   - `=` (Assignment)
+   - `+=` (Addition assignment)
+   - `-= ` (Subtraction assignment)
+   - `*=` (Multiplication assignment)
+   - `/=` (Division assignment)
+   - `%= ` (Modulus assignment)
+   - `**=` (Exponentiation assignment)
+   - `//=` (Floor division assignment)
+
+   Ex:-
+   x = 5
+   x += 3  # Equivalent to x = x + 3
+   print(x)  # Output: 8
+   
+
+5. **Bitwise Operators:**
+   - `&` (Bitwise AND)
+   - `|` (Bitwise OR)
+   - `^` (Bitwise XOR)
+   - `~` (Bitwise NOT)
+   - `<<` (Left shift)
+   - `>>` (Right shift)
+
+   Ex:-
+   a = 5  # Binary: 0101
+   b = 3  # Binary: 0011
+   print(a & b)  # Output: 1 (Bitwise AND)
+   print(a | b)  # Output: 7 (Bitwise OR)
+   
+4] Python Functions
+->
+'''
